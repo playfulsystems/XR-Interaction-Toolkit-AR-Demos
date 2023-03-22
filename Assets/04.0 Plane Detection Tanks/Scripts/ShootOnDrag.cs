4df3bdb8ec873fd287d958e32183a47e28f73ba2
Jonah Warren <jonah@playfulsystems.com>
2023-03-21 21:38:00 -0400

    select tanks before shooting, add select code

--- Assets/04.0 Plane Detection Tanks/Scripts/ShootOnDrag.cs
+++ Assets/04.0 Plane Detection Tanks/Scripts/ShootOnDrag.cs
@@ -1,39 +1,67 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
 using UnityEngine.XR.Interaction.Toolkit.AR;
 
 public class ShootOnDrag : MonoBehaviour
 {
     public GameObject bulletPrefab;
     public GameObject bulletStartPos;
-    public ARGestureInteractor arGestureInteractor;
+    ARGestureInteractor arGestureInteractor;
+    ARSelectionInteractable arSelectionInteractable;
     Vector2 startDragPos;
 
-    void Start()
+    void OnEnabled()
     {
         arGestureInteractor = Camera.main.GetComponent<ARGestureInteractor>();
         arGestureInteractor.dragGestureRecognizer.onGestureStarted += OnDragRecognized;
+
+        arSelectionInteractable = GetComponent<ARSelectionInteractable>();
+
+        // if you wanted to execute code on select
+        //arSelectionInteractable.selectEntered.AddListener(OnSelectTank);
     }
+
+    // if you wanted to execute code on select
+    //private void OnSelectTank(SelectEnterEventArgs args)
+	//{
+	//}
+
+	void OnDisabled()
+    {
+        arGestureInteractor.dragGestureRecognizer.onGestureStarted -= OnDragRecognized;
+    }
+
     private void OnDragRecognized(DragGesture obj)
     {
         Debug.Log("DRAG START");
-        startDragPos = obj.position;
-        GetComponent<RotateScript>().isRotating = false;
-        obj.onFinished += OnDragComplete;
+
+        if (arSelectionInteractable.isSelected)
+		{
+            startDragPos = obj.position;
+            GetComponent<RotateScript>().isRotating = false;
+            obj.onFinished += OnDragComplete;
+        }
     }
+
     private void OnDragComplete(DragGesture obj)
     {
         Debug.Log("DRAG COMPLETE");
-        GetComponent<RotateScript>().isRotating = true;
 
-        Vector2 diffVector = obj.position - startDragPos;
-        GameObject newBullet = Instantiate(bulletPrefab);
-        newBullet.transform.position = bulletStartPos.transform.position;
+        if (arSelectionInteractable.isSelected)
+        {
+            GetComponent<RotateScript>().isRotating = true;
 
-        Vector3 bulletForce = transform.forward + new Vector3(0f, 1f, 0f);
-        bulletForce *= (diffVector.magnitude * 0.01f);
-        newBullet.GetComponent<Rigidbody>().AddForce(bulletForce, ForceMode.Impulse);
+            Vector2 diffVector = obj.position - startDragPos;
+            GameObject newBullet = Instantiate(bulletPrefab);
+            newBullet.transform.position = bulletStartPos.transform.position;
+
+            Vector3 bulletForce = transform.forward + new Vector3(0f, 1f, 0f);
+            bulletForce *= (diffVector.magnitude * 0.01f);
+            newBullet.GetComponent<Rigidbody>().AddForce(bulletForce, ForceMode.Impulse);
+        }
     }
 
 }
--- Assets/04.0 Plane Detection Tanks/Scripts/Vector2Extensions.cs
+++ Assets/04.0 Plane Detection Tanks/Scripts/Vector2Extensions.cs
@@ -7,12 +7,6 @@ public static class Vector2Extensions
 {
 	public static bool IsPointOverUIObject(this Vector2 pos)
 	{
-		// if the pointer is over a gameobject and it isn't on the UI layer
-		//if (EventSystem.current.IsPointerOverGameObject())
-		//{
-		//	return false;
-		//}
-
 		// if it's over something else return true
 		PointerEventData eventDataCurrentPosition = new PointerEventData(EventSystem.current);
 		eventDataCurrentPosition.position = new Vector2(pos.x, pos.y);
@@ -23,7 +17,6 @@ public static class Vector2Extensions
 		int uiObjects = 0; // only count things not on the UI layer
 		for (int i = 0; i < results.Count; i++)
 		{
-			//Debug.Log("hit ("+i+") " + results[0].gameObject.name);
 			if (results[0].gameObject.layer == 5) uiObjects++; // 5 is the UI layer
 		}
 
